3f21ca1496cf1014d3d93a9e3323cefe927b4a77
Alex Collins <alexander-collins@outlook.com>
2021-08-23 16:15:01 +0100

    implemented active req limit(?)

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ const rngLat = maxLat - minLat
 const maxLng = 180, minLng = -180
 const rngLng = maxLng - minLng
 
+const nReqs = 10
+var reqs = []
+for (let i = 0; i < nReqs; i++) {
+	let lat = (Math.random() * rngLat) - maxLat
+	let lng = (Math.random() * rngLng) - maxLng
+	reqs.push(fetchData(lat, lng).catch((err) => {
+		console.warn(err)
+	}))
+}
+
+makeRequests(reqs).then((dataset) => {
+	if (dataset.length > 0) {
+		let earliest = 0
+		for (let i = 1; i < dataset.length; i++) {
+			if (Date.parse(dataset[earliest].sunrise) < Date.parse(dataset[i].sunrise))
+				earliest = i
+		}
+		console.log(`EARLIEST ${earliest}
+		sunrise: ${dataset[earliest].sunrise}
+		day length: ${dataset[earliest].day_length}`)
+	} else {
+		console.warn('dataset is empty!')
+	}
+})
+
 function fetchData(lat, lng, date) {
 	if (lat == undefined || lng == undefined)
 		throw new Error(`invalid lat/lng params passed to fetchData(${lat}, ${lng}, ...)`)
@@ -24,28 +49,19 @@ function fetchData(lat, lng, date) {
 						reject(new Error(`invalid status (${data.status}) from ${url}`))
 					resolve(data.results)
 				} catch (err) {
+					reject(err)
 				}
 			})
-		})
+		}).on('error', (err) => { reject(Error(`${url}\nfailed ${err.message}`)) }).end()
 	})
 }
 
-const nReqs = 5
-var reqs = []
-for (let i = 0; i < nReqs; i++) {
-	let lat = (Math.random() * rngLat) - maxLat
-	let lng = (Math.random() * rngLng) - maxLng
-	reqs.push(fetchData(lat, lng).catch((err) => {
-		console.error(err)
-	}))
-}
-Promise.all(reqs).then((dataset) => {
-	let earliest = 0
-	for (let i = 1; i < dataset.length; i++) {
-		if (Date.parse(dataset[earliest].sunrise) < Date.parse(dataset[i].sunrise))
-			earliest = i
+async function makeRequests(requests) {
+	const maxActive = 5
+	var dataset = []
+	for (let i = 0; i < maxActive; i += maxActive) {
+		data = await Promise.all(reqs.slice(i, i + maxActive)).catch(console.error)
+		dataset.concat(data)
 	}
-	console.log(`EARLIEST ${earliest}
-	sunrise: ${dataset[earliest].sunrise}
-	day length: ${dataset[earliest].day_length}`)
-})
+	return dataset
+}
